a47ed72e8cda9de98bbd3b0552cba465983eaf84
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-04-21 19:51:01 -0500

    add skeleton for high-level charts

---
+++ lib/Graphics/Bokeh/Charts.pm
@@ -0,0 +1,19 @@
+package Graphics::Bokeh::Charts;
+
+use strict;
+use warnings;
+
+package Graphics::Bokeh::Chart::Area       { ...  };
+package Graphics::Bokeh::Chart::Bar        { ...  };
+package Graphics::Bokeh::Chart::BoxPlot    { ...  };
+package Graphics::Bokeh::Chart::Donut      { ...  };
+package Graphics::Bokeh::Chart::Dot        { ...  };
+package Graphics::Bokeh::Chart::HeatMap    { ...  };
+package Graphics::Bokeh::Chart::Histogram  { ...  };
+package Graphics::Bokeh::Chart::Horizon    { ...  };
+package Graphics::Bokeh::Chart::Line       { ...  };
+package Graphics::Bokeh::Chart::Scatter    { ...  };
+package Graphics::Bokeh::Chart::Step       { ...  };
+package Graphics::Bokeh::Chart::Timeseries { ...  };
+
+1;
