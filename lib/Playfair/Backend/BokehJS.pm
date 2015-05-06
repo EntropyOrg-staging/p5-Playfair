13552300574523f3e2b74c899a58483cb1a1c4e8
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-05-06 10:32:33 -0500

    skeleton for BokehJS figure

---
+++ lib/Playfair/Backend/BokehJS.pm
@@ -0,0 +1,9 @@
+package Playfair::Backend::BokehJS;
+
+use strict;
+use warnings;
+
+sub compile {
+}
+
+1;
---
+++ lib/Playfair/Backend/BokehJS/Figure.pm
@@ -0,0 +1,12 @@
+package Playfair::Backend::BokehJS::Figure;
+
+use strict;
+use warnings;
+
+use Moo;
+
+has annotations => ( is => 'rw' );
+
+has glyphs => ( is => 'rw' );
+
+1;
